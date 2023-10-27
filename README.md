0129c9eb811130117ebdfb7d675f55fad51966e4
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-27 15:43:04 -0300

    Working on gallery

--- README.md
+++ README.md
@@ -40,3 +40,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 mrsiraphol: https://www.freepik.com/author/mrsiraphol
 lifeforstock: https://www.freepik.com/author/lifeforstock
 freepik: https://www.freepik.com/author/freepik
+Xavier Coiffic: https://unsplash.com/es/@xaviercoiffic
+Benjamin Voros: https://unsplash.com/es/@vorosbenisop
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -9,6 +9,8 @@ import gallery7 from '../../../public/img/gallery/gallery7.webp'
 import gallery8 from '../../../public/img/gallery/gallery8.webp'
 import gallery9 from '../../../public/img/gallery/gallery9.webp'
 import gallery10 from '../../../public/img/gallery/gallery10.webp'
+import gallery11 from '../../../public/img/gallery/gallery11.webp'
+import gallery12 from '../../../public/img/gallery/gallery12.webp'
 import './page.scss'
 
 export default function Gallery() {
@@ -17,7 +19,7 @@ export default function Gallery() {
       <div className='columns'>
         <Column className={`column column-reverse`} src1={gallery1} src2={gallery2} src3={gallery3} src4={gallery4} />
         <Column className={`column`} src1={gallery5} src2={gallery6} src3={gallery7} src4={gallery8} />
-        <Column className={`column column-reverse`} src1={gallery9} src2={gallery10} src3={gallery3} src4={gallery4} />
+        <Column className={`column column-reverse`} src1={gallery9} src2={gallery10} src3={gallery11} src4={gallery12} />
       </div>
     </>
   )
