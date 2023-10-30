ff308af823b6ebf3967841ccd0fd1bf148980eb9
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-30 16:26:30 -0300

    UI tweaks

--- app/gallery/column/column.js
+++ app/gallery/column/column.js
@@ -1,12 +1,13 @@
 import ColumnItem from '../column-item/column-item'
 
-function Column({ className, src1, src2, src3, src4 }) {
+function Column({ className, src1, src2, src3, src4, src5 }) {
   return (
     <div className={className}>
       <ColumnItem src={src1} />
       <ColumnItem src={src2} />
       <ColumnItem src={src3} />
       <ColumnItem src={src4} />
+      <ColumnItem src={src5} />
     </div>
   )
 }
--- app/gallery/page.js
+++ app/gallery/page.js
@@ -11,15 +11,18 @@ import gallery9 from '@/public/img/gallery/gallery9.webp'
 import gallery10 from '@/public/img/gallery/gallery10.webp'
 import gallery11 from '@/public/img/gallery/gallery11.webp'
 import gallery12 from '@/public/img/gallery/gallery12.webp'
+import gallery13 from '@/public/img/gallery/gallery13.webp'
+import gallery14 from '@/public/img/gallery/gallery14.webp'
+import gallery15 from '@/public/img/gallery/gallery15.webp'
 import './page.scss'
 
 export default function Gallery() {
   return (
     <>
       <div className='columns'>
-        <Column className={`column column-reverse`} src1={gallery1} src2={gallery2} src3={gallery3} src4={gallery4} />
-        <Column className={`column`} src1={gallery5} src2={gallery6} src3={gallery7} src4={gallery8} />
-        <Column className={`column column-reverse`} src1={gallery9} src2={gallery10} src3={gallery11} src4={gallery12} />
+        <Column className={`column column-reverse`} src1={gallery1} src2={gallery2} src3={gallery3} src4={gallery4} src5={gallery13} />
+        <Column className={`column`} src1={gallery5} src2={gallery6} src3={gallery7} src4={gallery8} src5={gallery14} />
+        <Column className={`column column-reverse`} src1={gallery9} src2={gallery10} src3={gallery11} src4={gallery12} src5={gallery15} />
       </div>
     </>
   )
