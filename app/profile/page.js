98e0e0c6883d16c2a9f3f7333ef430f0dfc5f171
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-11-06 17:04:06 -0300

    Delete account function added

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,8 +1,10 @@
 'use client'
 
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { Context } from '@/app/context/context'
 import { useRouter } from 'next/navigation'
+import { auth } from '@/app/firebase/firebase'
+import { deleteUser } from 'firebase/auth'
 import styles from './page.module.scss'
 
 export default function Profile() {
@@ -16,12 +18,64 @@ export default function Profile() {
     }
   }, [session])
 
-  if (session.emailVerified) {
+  const { edit, setEdit } = useState(false)
+
+  const handleEdit = () => {
+    setEdit(true)
+  }
+
+  const Swal = require('sweetalert2')
+
+  const handleDelete = () => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You won\'t be able to revert this, but you can create a new account using the same email.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#dc3545',
+      cancelButtonColor: '#6c757d',
+      confirmButtonText: 'Delete'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const user = auth.currentUser
+        deleteUser(user).then(() => {
+          // User deleted.
+          Swal.fire({
+            title: 'Deleted!',
+            text: 'Your account has been deleted.',
+            icon: 'success'
+          })
+        }).catch((error) => {
+          // An error ocurred
+          console.log(error)
+          // ...
+        });
+
+      }
+    })
+  }
+
+  if (session.emailVerified && !edit) {
     return (
       <main className={styles.main}>
-        <div className='fs-2 mb-5'>Profile</div>
-        <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure officiis rerum vitae. Cumque animi minus maiores. Mollitia incidunt laudantium voluptas illum, nobis earum quos nam omnis impedit, non, corporis veritatis!</div>
-      </main>
+        <h1 className='fs-2 mb-5'>Profile</h1>
+
+        <div className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}>
+          <span className={styles.left}>Name</span>
+          <span className={styles.right}>{session.displayName}</span>
+        </div>
+
+        <div className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}>
+          <span className={styles.left}>Email</span>
+          <span className={styles.right}>{session.email}</span>
+        </div>
+
+        <div className='mt-5'>
+          <button className='btn btn-sm btn-danger mx-2' onClick={handleDelete}>Delete account</button>
+          <button className='btn btn-sm btn-secondary mx-2' onClick={handleEdit}>Edit information</button>
+        </div>
+
+      </main >
     )
   } if (session) {
     return (
