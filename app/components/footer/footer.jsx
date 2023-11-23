e79d74f1df23458c4d2acec82bd22bf480a4f9da
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-11-23 10:52:39 -0300

    Migrating to React Bootstrap

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -1,14 +1,14 @@
 import { Allison } from 'next/font/google'
 import { CCircle } from 'react-bootstrap-icons'
 import styles from './footer.module.scss'
+import { Container } from 'react-bootstrap'
 
 const allison = Allison({ subsets: ['latin'], weight: ['400'] })
 
 export default function Footer() {
   return (
     <footer data-bs-theme='dark' className={`py-3 ${styles.footer}`}>
-
-      <div className={`container d-flex flex-wrap justify-content-evenly align-items-center`}>
+      <Container className='d-flex justify-content-evenly align-content-center'>
 
         <div className='col-md-4 d-flex flex-column mt-1 text-center'>
           <p className='mb-0'>
@@ -20,7 +20,7 @@ export default function Footer() {
 
         <a href='#body' className={`${allison.className} col-md-4 d-flex justify-content-center text-decoration-none fs-1`}>M</a>
 
-      </div>
+      </Container>
 
     </footer>
   )
