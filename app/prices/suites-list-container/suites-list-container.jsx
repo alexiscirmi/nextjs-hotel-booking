316d49a17eef344681751680d70bae49a2998d6c
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-11-24 19:35:58 -0300

    Code cleanup

--- app/prices/suites-list-container/suites-list-container.jsx
+++ app/prices/suites-list-container/suites-list-container.jsx
@@ -3,27 +3,21 @@ import Suite from './suite/suite'
 
 export default async function SuitesListContainer() {
 
-  try {
-    const file = await fs.readFile(process.cwd() + '/public/json/suites.json', 'utf8')
-    const suites = await JSON.parse(file)
-    console.log(suites)
+  const file = await fs.readFile(process.cwd() + '/public/json/suites.json', 'utf8')
+  const suites = await JSON.parse(file)
 
-    return (
-
-      <div className='d-flex flex-wrap justify-content-evenly'>
-        {suites.map((suite) =>
-          <Suite
-            key={suite.id}
-            id={suite.id}
-            image1={suite.images[0].image1}
-            name={suite.name}
-            price={suite.price}
-            available={suite.available}
-          />)}
-      </div>
-    )
-  } catch (error) {
-    console.log(error)
-  }
+  return (
+    <div className='d-flex flex-wrap justify-content-evenly'>
+      {suites.map((suite) =>
+        <Suite
+          key={suite.id}
+          id={suite.id}
+          image1={suite.images[0].image1}
+          name={suite.name}
+          price={suite.price}
+          available={suite.available}
+        />)}
+    </div>
+  )
 
 }
