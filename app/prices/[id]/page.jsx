70a9f53b7362f6abf6bf294fc4ff11d8e20af1ac
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-11-25 00:32:08 -0300

    Minor changes

--- app/prices/[id]/page.jsx
+++ app/prices/[id]/page.jsx
@@ -1,23 +1,17 @@
 import SuiteDetail from './suite-detail/suite-detail'
 import styles from './page.module.scss'
-
 import { promises as fs } from 'fs'
 
 export default async function SuiteDetailContainer({ params }) {
 
-  try {
-    const { id } = params
-    const file = await fs.readFile(process.cwd() + '/public/json/suites.json', 'utf8')
-    const suites = await JSON.parse(file)
-    const suite = suites.find(object => object.id === parseInt(id))
+  const { id } = params
+  const file = await fs.readFile(process.cwd() + '/public/json/suites.json', 'utf8')
+  const suites = await JSON.parse(file)
+  const suite = suites.find(object => object.id == id)
 
-    return (
-      <main className={styles.main}>
-        <SuiteDetail suite={suite} />
-      </main>
-    )
-  } catch (error) {
-    console.log(error)
-  }
-
+  return (
+    <main className={styles.main}>
+      <SuiteDetail suite={suite} />
+    </main>
+  )
 }
