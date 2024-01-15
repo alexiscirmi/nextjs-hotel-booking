10de072cfe7e6c9814513604677626793ea3577e
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2024-01-15 20:21:46 -0300

    Link updated

--- app/components/footer/footer.jsx
+++ app/components/footer/footer.jsx
@@ -9,19 +9,28 @@ export default function Footer() {
   return (
     <footer data-bs-theme='dark' className={`py-3 ${styles.footer}`}>
       <Container className='d-flex justify-content-evenly align-content-center'>
-
         <div className='col-md-4 d-flex flex-column mt-1 text-center'>
           <p className='mb-0'>
-            <a href='https://github.com/cirmialexis/nextjs-hotel-booking' target='_blank' rel='noreferrer' className='nav-link-footer pe-1'>Alexis Cirmi</a>
+            <a
+              href='https://github.com/alexiscirmi/nextjs-hotel-booking'
+              target='_blank'
+              rel='noreferrer'
+              className='nav-link-footer pe-1'
+            >
+              Alexis Cirmi
+            </a>
             <CCircle className={styles.biCCircle} />
           </p>
           <p className='m-0'>fantasy site</p>
         </div>
 
-        <a href='#body' className={`${allison.className} col-md-4 d-flex justify-content-center text-decoration-none fs-1`}>M</a>
-
+        <a
+          href='#body'
+          className={`${allison.className} col-md-4 d-flex justify-content-center text-decoration-none fs-1`}
+        >
+          M
+        </a>
       </Container>
-
     </footer>
   )
 }
