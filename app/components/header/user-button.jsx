27404eaa1ddc296bcf34bdaea303e83a509bb967
alexiscirmi <1320538+alexiscirmi@users.noreply.github.com>
2024-03-11 00:17:41 -0300

    UI improvements

--- app/components/header/user-button.jsx
+++ app/components/header/user-button.jsx
@@ -26,35 +26,68 @@ export default function UserButton() {
     return <Loading className='text-white' />
   } else {
     if (session) {
-      return (
-        <DropdownButton
-          as={ButtonGroup}
-          key='down-centered'
-          drop='down-centered'
-          align='end'
-          variant='light'
-          data-bs-theme='dark'
-          title={
-            session.email.slice(0, session.email.indexOf('@')).length <= 15
-              ? session.email.slice(0, session.email.indexOf('@'))
-              : `${session.email.slice(0, 10)}...`
-          }
-        >
-          <li>
-            <Link href='/profile' className='dropdown-item my-3 link-light'>
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Button
-              onClick={handleSignOut}
-              className='dropdown-item my-3 link-light'
-            >
-              Sign out
-            </Button>
-          </li>
-        </DropdownButton>
-      )
+      if (session.displayName.length > 0) {
+        return (
+          <DropdownButton
+            as={ButtonGroup}
+            key='down-centered'
+            drop='down-centered'
+            align='end'
+            variant='light'
+            data-bs-theme='dark'
+            title={
+              session.displayName.slice(0, session.displayName.indexOf('@'))
+                .length <= 9
+                ? session.displayName.slice(0, 10)
+                : `${session.displayName.slice(0, 10)}...`
+            }
+          >
+            <li>
+              <Link href='/profile' className='dropdown-item my-3 link-light'>
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Button
+                onClick={handleSignOut}
+                className='dropdown-item my-3 link-light'
+              >
+                Sign out
+              </Button>
+            </li>
+          </DropdownButton>
+        )
+      } else {
+        return (
+          <DropdownButton
+            as={ButtonGroup}
+            key='down-centered'
+            drop='down-centered'
+            align='end'
+            variant='light'
+            data-bs-theme='dark'
+            title={
+              session.email.slice(0, session.email.indexOf('@')).length <= 15
+                ? session.email.slice(0, session.email.indexOf('@'))
+                : `${session.email.slice(0, 10)}...`
+            }
+          >
+            <li>
+              <Link href='/profile' className='dropdown-item my-3 link-light'>
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Button
+                onClick={handleSignOut}
+                className='dropdown-item my-3 link-light'
+              >
+                Sign out
+              </Button>
+            </li>
+          </DropdownButton>
+        )
+      }
     } else {
       return (
         <Link href='/auth' className='btn btn-outline-light'>
--- app/profile/info.jsx
+++ app/profile/info.jsx
@@ -3,21 +3,26 @@ import { getDocs, query, collection, where } from 'firebase/firestore'
 import DeleteButton from './buttons/delete-button'
 import EditButton from './buttons/edit-button'
 import styles from './page.module.scss'
+import Loading from '../components/loading/loading'
 
 export default function Info({ auth, session, db, setEdit }) {
-
   const [reservations, setReservations] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const reservationsCollection = await getDocs(
-          query(collection(db, 'reservations'), where('email', '==', session.email))
+          query(
+            collection(db, 'reservations'),
+            where('email', '==', session.email)
+          )
         )
-        const reservationsData = reservationsCollection.docs.map(doc => ({
+        const reservationsData = reservationsCollection.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data(),
+          ...doc.data()
         }))
+        setLoading(false)
         setReservations(reservationsData)
       } catch (error) {
         console.error('Error fetching data:', error)
@@ -25,25 +30,33 @@ export default function Info({ auth, session, db, setEdit }) {
     }
 
     fetchData()
-  }, [])
+  }, [db, session.email])
 
   return (
     <>
-      <div className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}>
+      <div
+        className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}
+      >
         <label className={styles.left}>Name</label>
         <span className={styles.right}>{session.displayName}</span>
       </div>
 
-      <div className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}>
+      <div
+        className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}
+      >
         <label className={styles.left}>Email</label>
         <span className={styles.right}>{session.email}</span>
       </div>
 
-      <div className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}>
+      <div
+        className={`rounded border border-secondary border-opacity-25 my-1 ${styles.detail}`}
+      >
         <label className={styles.left}>Reservations</label>
         <div className={styles.right}>
-          {reservations.length > 0
-            ? (<ul>
+          {loading ? (
+            <Loading className='my-1' />
+          ) : reservations.length > 0 ? (
+            <ul>
               {reservations.map((reservation, index) => (
                 <li className='my-1 text-start' key={index}>
                   <p className='mb-0'>#{reservation.id}</p>
@@ -52,10 +65,10 @@ export default function Info({ auth, session, db, setEdit }) {
                   <p className='mb-0'>Suite: {reservation.suite}</p>
                 </li>
               ))}
-            </ul>)
-            : (<span>No reservations found</span>)
-          }
-
+            </ul>
+          ) : (
+            <span>No reservations found</span>
+          )}
         </div>
       </div>
 
